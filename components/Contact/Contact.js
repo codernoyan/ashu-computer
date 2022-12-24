de9323059ec309ae815d4469944c4e8fc4086513
codernoyan <coder.noyan@gmail.com>
2022-12-24 14:33:31 +0600

    project completed

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import React from 'react';
-import contactAnimation from '../../public/contact.gif';
+import contactAnimation from '../../public/contact-2.gif';
 
 const Contact = () => {
   return (
@@ -9,18 +9,18 @@ const Contact = () => {
         <h1 className="text-center text-4xl font-semibold">আমাদের সাথে <span className="text-cyan-500">যোগাযোগ</span></h1>
       </div>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 justify-items-center md:justify-items-between items-center text-center py-16">
-        <div>
-          <Image src={contactAnimation} height={400} width={400} alt="contact"></Image>
+        <div className="justify-self-center md:justify-self-start">
+          <Image src={contactAnimation} height={500} width={500} alt="contact"></Image>
         </div>
-        <div className="text-base md:text-2xl space-y-4">
+        <div className="text-base md:text-2xl space-y-4 justify-self-center md:justify-self-end">
           <div>
             <p className="text-cyan-500">ঠিকানা - </p>
             <p>আব্দুল রাজ্জাক মার্কেট, শান্তিরহাট</p>
             <p>(কাপ্তাই রোড সংলগ্ন), পোমরা, রাঙ্গুনিয়া, চট্টগ্রাম</p>
           </div>
           <div>
-            <p><span className="text-cyan-500">ফোন -</span> (+৮৮) ০১৮১২৫৪১২১৫ | +8801812-541215</p>
-            <p><span className="text-cyan-500">ই-মেইল -</span> ashucomputer@gmail.com</p>
+            <p><span className="text-cyan-500">ফোন -</span> <a href="tel:+8801812541215">(+৮৮) ০১৮১২৫৪১২১৫</a> | <a href="tel:+8801812541215">+8801812-541215</a></p>
+            <p><span className="text-cyan-500">ই-মেইল -</span> <a href="mailto:ashucomputer@gmail.com">ashucomputer@gmail.com</a></p>
           </div>
         </div>
       </div>
--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer aria-label="Site Footer">
-      <div className="mx-auto max-w-screen-xl px-4 py-4 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-screen-xl px-2 py-4 sm:px-6 lg:px-0">
         <div className="sm:flex sm:items-center sm:justify-between">
           <div className="flex justify-center text-teal-600 sm:justify-start">
             <Link href="/"
